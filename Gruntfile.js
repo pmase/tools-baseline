dc7b4c0c48bb9fdd5f6360daefdbdb9518f8074e
Nicholas Bollweg <nicholas.bollweg@gtri.gatech.edu>
2013-08-20 14:23:27 -0400

    cleaning out things from reference implementation

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,34 +2,8 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      test_steps_js: {
-        src: [
-          'features/dictionary.js',
-          'features/library.js',
-          'features/steps/**/*.js'
-        ],
-        dest: 'test/steps.js'
-      },
-      test_features: {
-        src: ['features/**/*.feature'],
-        dest: 'test/features.feature'
-      },
-      test_mocha_js: {
-        src: ['node_modules/mocha/mocha.js'],
-        dest: 'test/lib/mocha.js',
-      },
-      test_mocha_css: {
-        src: ['node_modules/mocha/mocha.css'],
-        dest: 'test/lib/mocha.css',
-      },
-      test_yadda: {
-        src: ['node_modules/yadda/dist/yadda-0.4.5.js'],
-        dest: 'test/lib/yadda.js',
-      }
-    },
     jshint: {
-      files: ['Gruntfile.js', 'features/**/*.js'],
+      files: ['Gruntfile.js'],
       // configure JSHint (documented at http://www.jshint.com/docs/)
       options: {
         // options here to override JSHint defaults
@@ -45,34 +19,17 @@ module.exports = function(grunt) {
           spawn: false,
         },
       },
-    },
-    mocha: {
-      runner: {
-        src: ['test/runner.html'],
-        
-        options: {
-          // Pipe output console.log from your JS to grunt. False by default.
-          log: true,
-          
-          // Override the timeout of the test (default is 5000)
-          timeout: 10000,
-          
-          
-          // Select a Mocha reporter
-          // http://visionmedia.github.com/mocha/#reporters
-          reporter: 'Nyan'
-        }
-      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-mocha');
 
-  // add a real test framework here
-  grunt.registerTask('test', ['concat', 'mocha']);
+  // add a real test framework here...
+  //grunt.registerTask('test', ['concat', 'mocha']);
 
-  grunt.registerTask('default', ['jshint', 'test']);
+  // ...and then add the test task to the default task
+  //grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('default', ['jshint']);
 };
--- features/library.js
+++
@@ -1,7 +0,0 @@
-;(function(require, dictionary){
-  "use strict";
-  var yadda = require('yadda'),
-    library = new yadda.localisation.English(dictionary);
-    
-  this.library = library;
-}).call(this, require, dictionary);
